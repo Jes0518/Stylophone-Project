38a6ffecc6b519277e00fddd10bf1558370d6178
Jes0518 <josee.sanchez42@gmail.com>
2025-04-23 16:48:49 -0500

    File upload

---
+++ Final_Project/Core/Inc/app.h
@@ -0,0 +1,18 @@
+/*
+ * app.h
+ *
+ *  Created on: Jun 19, 2022
+ *      Author: xl0021
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "stm32l4xx_hal.h"
+
+
+void App_Init(void);
+void App_MainLoop(void);
+
+
+#endif /* INC_APP_H_ */
